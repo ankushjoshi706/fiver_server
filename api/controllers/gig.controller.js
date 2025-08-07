12f23987a5536e5742022ac39769f5c7665aab96
ankushjoshi706 <ankushjoshi706@gmail.com>
2025-08-08 05:14:32 +0530

    first commit
    first commit

---
+++ api/controllers/gig.controller.js
@@ -0,0 +1,65 @@
+import Gig from "../models/gig.model.js";
+import createError from "../utils/createError.js";
+
+
+
+export const createGig = async (req, res, next)=>{
+    //-----Only able to create the gig if user is seller-----
+    if(!req.isSeller) 
+        return next(createError(403, "Only sellers can create a gig!"));
+
+    const newGig = new Gig({
+        userId: req.userId,
+        ...req.body,
+    });
+
+    try{
+        const savedGig = await newGig.save();
+        res.status(201).json(savedGig);
+    }catch(err){
+        next(err);
+    }
+}
+
+export const deleteGig = async (req, res, next)=>{
+    try{
+        const gig = await Gig.findById(req.params.id);
+
+        if(gig.userId !== req.userId) return next(createError(403, "You can delete only Your gig!"));
+
+        await Gig.findByIdAndDelete(req.params.id);
+        res.status(200).send("Gig has been deleted!");
+    }catch(err){
+        next(err);
+    }
+}
+
+export const getGig = async (req, res, next)=>{
+    try{
+        const gig = await Gig.findById(req.params.id);
+        res.status(200).send(gig);
+
+        if(!gig) return next()
+
+    }catch(err){
+        next(err);
+    }
+}
+
+export const getGigs = async (req, res, next)=>{
+
+    const q = req.query;
+    //----------Filtering the serch for the Gigs------
+    const filters = {
+        ...(q.userId && {userId:q.userId}),
+        ...(q.cat && {cat:q.cat}),
+        ...((q.min || q.max) && {price: {...(q.min && {$gt: q.min}), ...Gig(q.max && {$lt: q.max})}}),
+        ...(q.search && {title:{$regex: q.search, $options: "i"}}),
+    }
+    try{
+        const gigs = await Gig.find(filters).sort({[q.sort]:-1});
+        res.status(200).send(gigs);
+    }catch(err){
+        next(err);
+    }
+}
---
+++ api/utils/createError.js
@@ -0,0 +1,11 @@
+//----------Creating the Specific error---
+const createError = (status, message) =>{
+    const err = new Error();
+    err.status = status;
+    err.message = message;
+
+    return err;
+    
+};
+
+export default createError;
