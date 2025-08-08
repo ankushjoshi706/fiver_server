0f0d1b4dc4b49800115cd0ed8dba7be22c710fc9
ankushjoshi706 <85740434+ankushjoshi706@users.noreply.github.com>
2025-08-08 10:33:16 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -28,7 +28,7 @@ const connect = async ()=>{
 }
 
 //------------All Middleware----
-app.use(cors({origin:"http://localhost:5173", credentials: true}));
+app.use(cors({origin:"https://fiver-client-frontend-h2gq.vercel.app/", credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
